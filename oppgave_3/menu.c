148f5cfb53e446c8b68bb29b9948678c454e01d2
mahoote <martin@teigen.biz>
2022-12-15 18:40:10 -0500

    Update menu

--- oppgave_3/menu.c
+++ oppgave_3/menu.c
@@ -5,8 +5,6 @@
 void printMenu()
 {
     printf("\nPlease select an option from the menu below:\n");
-    printf("1. Print 'Hello World'\n");
-    printf("2. Sum 2 + 2\n");
-    printf("3. Print reservations\n");
+    printf("1. Print reservations\n");
     printf("0. Exit menu\n");
 }
