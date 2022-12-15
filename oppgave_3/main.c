0ea4dffdfec0eba4bbda34b17ee7cb99d02e1e89
mahoote <martin@teigen.biz>
2022-12-15 17:39:59 -0500

    Create basic menu

--- oppgave_3/main.c
+++ oppgave_3/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+#include "include/menu.h"
+
+int main()
 {
-    printf("Hello World\n");
+    int option;
+
+    // Continue to run the program until the user selects the exit option
+    while (1)
+    {
+        printMenu();
+
+        // Get the user's input
+        printf("\nEnter your option: ");
+        scanf("%d", &option);
+
+        // Perform the selected option
+        switch (option)
+        {
+        case 1:
+            printf("\nHello World\n");
+            break;
+        case 2:
+            printf("\nThe result of 2 + 2 is %d\n", 2 + 2);
+            break;
+        case 3:
+            printf("\nExiting menu...\n");
+            return 0;
+        default:
+            printf("\nInvalid option. Please try again.\n");
+            break;
+        }
+    }
+
     return 0;
 }
---
+++ oppgave_3/menu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "include/menu.h"
+
+void printMenu()
+{
+    printf("\nPlease select an option from the menu below:\n");
+    printf("1. Print 'Hello World'\n");
+    printf("2. Sum 2 + 2\n");
+    printf("3. Exit menu\n");
+}
